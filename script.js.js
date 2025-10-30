150750984aa30312d9dd77d09f3ff8cbde19667a
AJIJAJAM <ajamajij152@gmail.com>
2025-10-30 17:13:01 +0530

    Add files via upload
    An Amazon Clone is a modern e-commerce web app that mimics Amazon’s core features like product listings, cart, user login, and payments. Built with React, Node.js, MongoDB, and Express, it ensures a smooth shopping experience with an easy-to-use admin dashboard.

---
+++ script.js.js
@@ -0,0 +1,9 @@
+<script>
+    const userLocation = "India"; // Replace with a dynamic location-checking script if needed
+    if (userLocation === "India") {
+        window.location.href = "https://www.amazon.in"
+    }
+</script>
+
+
+
